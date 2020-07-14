22c18fa80640b08b8620fd318428212584400abb
Carl Redding <credding07@gmail.com>
2020-07-14 19:29:42 -0400

    UPER framework being laid out. talking out what the problem is. then writing down what needs to be done. then how i'm going to do it.

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -4,9 +4,26 @@ Returns: an integer
 '''
 '''
 UPER UPER TIME
+so this function takes in a list of ints. aka a list[1,2,3,4,5,6]
+ of nums where every int shows up twice except one so like list[1,1,2,2,3,4,4,5,5,6,6]
+so i need to return the int that is only listed once in a list.
+"so what needs to be done"
+as an arr list being passed into single_number(arr): fn 
+i need to look over each value in that list.
+while doing so i need to be able to check each value and see if there are more than one of the same value.
+if there is only one of a certain value. then return it.
+"how am i going to do it?"
+i'm thinking that i need to loop over the data,
+so either a for or while loop -
+then check each value if it meets certain conditions.
+if elif else. most likely if and else will be needed.
+
 '''
 def single_number(arr):
     # Your code here
+
+
+
 
     pass
 
