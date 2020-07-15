bd0e2867f0095ae0d14b53e6d2f92a1ac5d11eee
Carl Redding <credding07@gmail.com>
2020-07-15 15:22:24 -0400

    completed the first solution. needed a lof of google for help.

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -17,22 +17,15 @@ i'm thinking that i need to loop over the data,
 so either a for or while loop -
 then check each value if it meets certain conditions.
 if elif else. most likely if and else will be needed.
-
-sorting can be applied here. iteration sort or binary search tree would work aqui.
-bubble sort as well.
-
-
 '''
+# dbl = []
 def single_number(arr):
     # Your code here
-    n = len(arr)
-    for i in range(0, n):
-        print(n[1])
-    # pass
-
-
+    return 2 * sum(set(arr)) - sum(arr)
+        
 if __name__ == '__main__':
     # Use the main function to test your implementation
     arr = [1, 1, 4, 4, 5, 5, 3, 3, 9, 0, 0]
-
+    
     print(f"The odd-number-out is {single_number(arr)}")
+    # print(arr)    
