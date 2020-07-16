e90c34437129cbd58760e4ff156197f7945968d1
Carl Redding <credding07@gmail.com>
2020-07-16 10:24:15 -0400

    understanding what exactly i have to do with product_of_all_numbers.py file. now just trying to make it happen.

--- product_of_all_other_numbers/product_of_all_other_numbers.py
+++ product_of_all_other_numbers/product_of_all_other_numbers.py
@@ -11,19 +11,53 @@ so you have to do some super tricky math here.
 example array of [1,7,3,4] 
 you would multiply each value in this array like so
 [7*3*4,1*3*4,1*7*4,1*7*3] so the pattern is obv when you see it
-"how am i going to get this done."
-a lot of googling
+"breakdown" this is where i talk to myself in my room.
+arr[1, 7, 3, 4]
+   arr[0]start=[1][2][3]
+   start arr[1] 7*3*4 = 84 mult all but index0
+   then arr[2] 1*3*4 = 12 mult all but index1
+   then arr[3] 1*7*4 = 28 mult all but index2
+   then arr[0] 1*7*3 = 21 mult all but index3
+so while you are on a specific arr[index] you multiply everything BUT itself.
+   a couple thoughts. slicing the array. 'to start' arr[1:]
+   so you multiply everything in the arr EXCEPT that specific index your pointing at.
+   
+   current = current index
+   loop over the data. index by index in the arr.
+   so if at currentIndex mult what is before :0 and after 0: it
+   store that to [] so you can get [120,60,40,30,24]
 
+# arr = [1, 2, 3, 4, 5]
+
+1 2*3*4*5 = 120
+2 1*3*4*5 = 60
+3 1*2*4*5 = 40
+4 1*2*3*5 = 30
+5 1*2*3*4 = 24
+now how do i multiply everything up.
 '''
-def product_of_all_other_numbers(arr):
-    # Your code here
 
-    pass
+def product_of_all_other_numbers(arr):
+#     # Your code here
+    # current = arr[0]
+    for i in range(1, len(arr) + 1):
+        cur_index = i
+        index_tot = cur_index
+        print(i)
+        for j in arr:
+            if cur_index[0] * cur_index[0:] and cur_index[:0]: 
+            # print(f' second {j}')
+                return cur_index[0]
+            
+        # if arr[i]:
+        #     print(arr[3])
+        # print(arr)
+#     pass
 
 
 if __name__ == '__main__':
-    # Use the main function to test your implementation
-    # arr = [1, 2, 3, 4, 5]
-    arr = [2, 6, 9, 8, 2, 2, 9, 10, 7, 4, 7, 1, 9, 5, 9, 1, 8, 1, 8, 6, 2, 6, 4, 8, 9, 5, 4, 9, 10, 3, 9, 1, 9, 2, 6, 8, 5, 5, 4, 7, 7, 5, 8, 1, 6, 5, 1, 7, 7, 8]
+#     # Use the main function to test your implementation
+    arr = [1, 2, 3, 4, 5]
+    # arr = [2, 6, 9, 8, 2, 2, 9, 10, 7, 4, 7, 1, 9, 5, 9, 1, 8, 1, 8, 6, 2, 6, 4, 8, 9, 5, 4, 9, 10, 3, 9, 1, 9, 2, 6, 8, 5, 5, 4, 7, 7, 5, 8, 1, 6, 5, 1, 7, 7, 8]
 
     print(f"Output of product_of_all_other_numbers: {product_of_all_other_numbers(arr)}")
